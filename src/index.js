e26c3fbb9b583bade5b65330cd325f8ceab85372
Victor Seleimend <sseleimend@gmail.com>
2025-05-16 19:11:20 -0300

    Added route and test to the default URL

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(`App listening on port number: ${port}`);
 });
