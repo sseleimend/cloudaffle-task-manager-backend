fabf9c0d10309957f2e049f47fb88b957c1e043c
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 17:00:42 -0300

    add createTaskValidator

--- src/tasks/tasks.routes.js
+++ src/tasks/tasks.routes.js
@@ -1,31 +1,22 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 const tasksController = require("./tasks.controller.js");
+const createTaskValidator = require("./validators/createTask.validator.js");
 const { StatusCodes } = require("http-status-codes");
 
 const tasksRouter = express.Router();
 
 tasksRouter.get("/tasks", tasksController.handleGetTasks);
 
-tasksRouter.post(
-  "/tasks",
-  [
-    body("title", "The title cannot be empty").notEmpty(),
-    body("title", "The title must be a string").isString(),
-    body("dueDate", "dueDate needs to be a valid ISO8601 string")
-      .notEmpty()
-      .isISO8601(),
-  ],
-  (req, res) => {
-    const result = validationResult(req);
+tasksRouter.post("/tasks", createTaskValidator, (req, res) => {
+  const result = validationResult(req);
 
-    if (result.isEmpty()) {
-      return tasksController.handlePostTasks(req, res);
-    } else {
-      res.status(StatusCodes.BAD_REQUEST).json(result.array());
-    }
+  if (result.isEmpty()) {
+    return tasksController.handlePostTasks(req, res);
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
   }
-);
+});
 
 tasksRouter.patch("/tasks", tasksController.handlePatchTasks);
 
---
+++ src/tasks/validators/createTask.validator.js
@@ -0,0 +1,26 @@
+const { body } = require("express-validator");
+
+const createTaskValidator = [
+  body("title", "The title cannot be empty").notEmpty(),
+  body("title", "The title must be a string").isString(),
+  body("title").isLength({ max: 100 }),
+  body("title").trim(),
+  body(
+    "description",
+    "The description cannot be empty and needs to be a string"
+  )
+    .notEmpty()
+    .isString()
+    .trim(),
+  body(
+    "description",
+    "The description cannot be more than 500 characters"
+  ).isLength({ max: 500 }),
+  body("dueDate", "dueDate needs to be a valid ISO8601 string")
+    .notEmpty()
+    .isISO8601(),
+  body("priority").isIn(["low", "normal", "high"]),
+  body("status").isIn(["todo", "inProgress", "completed"]),
+];
+
+module.exports = createTaskValidator;
