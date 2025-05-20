f545a88b67877b0574c917cef4ada5bf3206c751
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 17:15:35 -0300

    Update createTaskProvider to handle errors in interactions with the database

--- src/tasks/providers/createTask.provider.js
+++ src/tasks/providers/createTask.provider.js
@@ -1,10 +1,18 @@
+const { StatusCodes } = require("http-status-codes");
 const Task = require("../task.schema.js");
 const { matchedData } = require("express-validator");
 
 async function createTaskProvider(req, res) {
   const validatedResult = matchedData(req);
   const task = new Task(validatedResult);
-  return await task.save();
+  try {
+    await task.save();
+    return res.status(StatusCodes.CREATED).json(task);
+  } catch (error) {
+    return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
+  }
 }
 
 module.exports = createTaskProvider;
