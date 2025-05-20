a283735593167c8ed2ea9969ba31808cfd5f6e0b
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 09:24:14 -0300

    Added cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
+const cors = require("cors");
 const tasksRouter = require("./tasks/tasks.router.js");
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: ["localhost"],
+};
+
+app.use(cors(corsOptions));
 
 let accessLogStream = fs.createWriteStream(
   path.join(__dirname, "..", "access.log"),
