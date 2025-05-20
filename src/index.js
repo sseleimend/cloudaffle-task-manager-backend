05a1d645a455da687c621bf120ee9534cd7660d7
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 08:29:47 -0300

    Update index.js by linking the task router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const tasksRouter = require("./tasks/tasks.router.js");
+
 const app = express();
 const port = 3001;
+
+app.use("/", tasksRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port number: ${port}`);
