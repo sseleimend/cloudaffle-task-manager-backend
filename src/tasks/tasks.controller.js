ca75eff27aa4e4bb0b15cabaa68e6ae9ffe3497d
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 13:46:11 -0300

    Update handlePostTasks to save the task in the database

--- src/tasks/tasks.controller.js
+++ src/tasks/tasks.controller.js
@@ -1,4 +1,5 @@
-const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
+const Task = require("./task.schema.js");
 
 function handleGetTasks(req, res) {
   let response = [
@@ -23,8 +24,18 @@ function handleGetTasks(req, res) {
   res.status(StatusCodes.OK).json(response);
 }
 
-function handlePostTasks(req, res) {
-  res.send("POST tasks controller");
+async function handlePostTasks(req, res) {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    priority: req.body.priority,
+    dueDate: req.body.dueDate,
+  });
+
+  await task.save();
+
+  res.status(StatusCodes.CREATED).json(task);
 }
 
 function handlePatchTasks(req, res) {
