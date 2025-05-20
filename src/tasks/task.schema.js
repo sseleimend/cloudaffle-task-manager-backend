4dd363e5bc6e2fb19ad65bff4278fc39a7261a02
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 13:34:00 -0300

    Add task schema

---
+++ src/tasks/task.schema.js
@@ -0,0 +1,39 @@
+const { Schema } = require("mongoose");
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Task title is required"],
+      trim: true,
+      maxLength: [100, "Title cannot be more than 100 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Task description is required"],
+      trim: true,
+      maxLength: [500, "Description cannot be more than 500 characters"],
+    },
+    status: {
+      type: String,
+      required: [true, "Task status is required"],
+      enum: ["todo", "inProgress", "completed"],
+      default: "todo",
+    },
+    priority: {
+      type: String,
+      required: [true, "Task priority is required"],
+      enum: ["low", "normal", "high"],
+      default: "normal",
+    },
+    dueDate: {
+      type: Date,
+      required: [true, "Task due date is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { timestamps: true }
+);
