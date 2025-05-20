0caa0f4f2bbb7df4f04dbf4e1983c4a129c84df2
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 17:28:01 -0300

    Add winston config

---
+++ src/helpers/winston.helper.js
@@ -0,0 +1,33 @@
+const winston = require("winston");
+const path = require("path");
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.printf(
+      (info) => `${info.timestamp} [${info.level}] : ${info.message}`
+    )
+  ),
+});
+
+const transports = [
+  new winston.transports.Console({
+    level: "info",
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+  }),
+  new winston.transports.File({
+    level: "info",
+    filename: path.join(__dirname, "../..", "info.log"),
+    format: winston.format.json(),
+  }),
+  new winston.transports.File({
+    level: "error",
+    filename: path.join(__dirname, "../..", "error.log"),
+    format: winston.format.json(),
+  }),
+];
