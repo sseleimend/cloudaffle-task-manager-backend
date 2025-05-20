8deddbb166d2398fd653207f8d4f3fe709f28428
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 18:10:28 -0300

    Add a proper error log in createTaskProvider

--- src/tasks/providers/createTask.provider.js
+++ src/tasks/providers/createTask.provider.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const Task = require("../task.schema.js");
 const { matchedData } = require("express-validator");
+const logger = require("../../helpers/winston.helper.js");
 
 async function createTaskProvider(req, res) {
   const validatedResult = matchedData(req);
@@ -9,6 +10,16 @@ async function createTaskProvider(req, res) {
     await task.save();
     return res.status(StatusCodes.CREATED).json(task);
   } catch (error) {
+    logger.error(`Error creating a new task: ${error.message}`, {
+      metadata: {
+        errorCode: error.code,
+        errorName: error.name,
+        method: req.method,
+        url: req.originalUrl,
+        body: req.body,
+        error: error,
+      },
+    });
     return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
       reason: "Unable to process your request at the moment, please try later.",
     });
