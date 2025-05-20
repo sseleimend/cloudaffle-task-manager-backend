da6c23146b15544e7d079869a40ab8c3bb38e168
Victor Seleimend <sseleimend@gmail.com>
2025-05-20 09:14:01 -0300

    Added morgan

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
 const tasksRouter = require("./tasks/tasks.router.js");
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+
+let accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "..", "access.log"),
+  {
+    flags: "a",
+  }
+);
+
+app.use(
+  morgan("combined", {
+    stream: accessLogStream,
+  })
+);
 
 app.use("/", tasksRouter);
 
