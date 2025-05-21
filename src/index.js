993b0b547a6ce4bdeb7d45051edb42a7a2f9d75b
Victor Seleimend <sseleimend@gmail.com>
2025-05-21 09:58:01 -0300

    Add a configuration file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const morgan = require("morgan");
-const cors = require("cors");
-const { StatusCodes } = require("http-status-codes");
-const responseFormatter = require("./middleware/responseFormatter.middleware.js");
-const tasksRouter = require("./tasks/tasks.routes.js");
-const authRouter = require("./auth/auth.routes.js");
-const usersRouter = require("./users/users.routes.js");
 const mongoose = require("mongoose");
-const expressWinstonLogger = require("./middleware/expressWinston.middleware.js");
 const dotenv = require("dotenv");
+const configureApp = require("./settings/config.js");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 const envFile = `.env.${process.env.NODE_ENV}`;
@@ -22,34 +13,7 @@ const port = parseInt(process.env.PORT);
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: ["localhost"],
-};
-
-app.use(cors(corsOptions));
-
-let accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "..", "access.log"),
-  {
-    flags: "a",
-  }
-);
-
-app.use(
-  morgan("combined", {
-    stream: accessLogStream,
-  })
-);
-app.use(responseFormatter);
-app.use(expressWinstonLogger);
-
-app.use("/", tasksRouter);
-app.use("/auth", authRouter);
-app.use("/users", usersRouter);
-
-app.use((req, res) => {
-  res.status(StatusCodes.NOT_FOUND).json(null);
-});
+configureApp(app);
 
 async function bootstrap() {
   try {
---
+++ src/settings/config.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+const { StatusCodes } = require("http-status-codes");
+const responseFormatter = require("../middleware/responseFormatter.middleware.js");
+const tasksRouter = require("../tasks/tasks.routes.js");
+const authRouter = require("../auth/auth.routes.js");
+const usersRouter = require("../users/users.routes.js");
+const expressWinstonLogger = require("../middleware/expressWinston.middleware.js");
+
+function configureApp(app) {
+  /* cors */
+  const corsOptions = {
+    origin: ["localhost"],
+  };
+  app.use(cors(corsOptions));
+
+  /* morgan */
+  let accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "../..", "access.log"),
+    {
+      flags: "a",
+    }
+  );
+  app.use(
+    morgan("combined", {
+      stream: accessLogStream,
+    })
+  );
+
+  /* other middlewares */
+  app.use(responseFormatter);
+  app.use(expressWinstonLogger);
+
+  /* define routes */
+  app.use("/", tasksRouter);
+  app.use("/auth", authRouter);
+  app.use("/users", usersRouter);
+
+  app.use((req, res) => {
+    res.status(StatusCodes.NOT_FOUND).json(null);
+  });
+}
+
+module.exports = configureApp;
