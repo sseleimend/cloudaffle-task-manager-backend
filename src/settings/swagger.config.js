43c054769debab6b95ec7c562f22fef8b3d77181
Victor Seleimend <sseleimend@gmail.com>
2025-05-21 15:44:46 -0300

    Add info and servers to the swagger configuration

--- src/settings/swagger.config.js
+++ src/settings/swagger.config.js
@@ -4,6 +4,25 @@ const path = require("path");
 const options = {
   definition: {
     openapi: "3.1.1",
+    info: {
+      title: "Task Manager API",
+      version: "1.0.0",
+      description: "Cloudaffle Task Manager Backend",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Victor Seleimend",
+        url: "https://google.com",
+        email: "sseleimend@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3001",
+      },
+    ],
   },
   apis: [path.join(__dirname, "..", "**/*.js")],
 };
