63757bb9d957c81341910467cf7373e2200e8b96
Victor Seleimend <sseleimend@gmail.com>
2025-05-21 16:14:53 -0300

    Add task schema documentation

--- src/tasks/task.schema.js
+++ src/tasks/task.schema.js
@@ -40,5 +40,46 @@ const taskSchema = new Schema(
 );
 
 const Task = model("Task", taskSchema);
-
 module.exports = Task;
+
+/**
+ * @swagger
+ *
+ * components:
+ *  schemas:
+ *    Task:
+ *      type: object
+ *      required:
+ *        - title
+ *        - description
+ *        - status
+ *        - priority
+ *        - dueDate
+ *      properties:
+ *        title:
+ *          type: string
+ *          description: The title of the task
+ *          maxLength: 100
+ *        description:
+ *          type: string
+ *          description: The description of the task
+ *          maxLength: 500
+ *        status:
+ *          type: string
+ *          description: The status of the task
+ *          enum: ["todo", "inProgress", "completed"]
+ *        priority:
+ *          type: string
+ *          description: The priority of the task
+ *          enum: ["low", "normal", "high"]
+ *        dueDate:
+ *          type: string
+ *          format: ISO8601 Date String
+ *          description: The due date for the task
+ *      example:
+ *        title: Create a new video
+ *        description: A video about fullstack web development
+ *        status: todo
+ *        priority: normal
+ *        dueDate: 2025-01-01T12:00:00Z
+ */
