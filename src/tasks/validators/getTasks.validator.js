2ad62661b0942952f98686d8b75e9002d4b2818d
Victor Seleimend <sseleimend@gmail.com>
2025-05-21 10:20:26 -0300

    Update the getTasks validator to have default values

--- src/tasks/validators/getTasks.validator.js
+++ src/tasks/validators/getTasks.validator.js
@@ -1,11 +1,20 @@
 const { query } = require("express-validator");
 
 const getTasksValidator = [
-  query("limit", "limit must be a valid integer").optional().isInt().toInt(),
-  query("page", "page must be a valid integer").optional().isInt().toInt(),
+  query("limit", "limit must be a valid integer")
+    .optional()
+    .isInt()
+    .toInt({ min: 1 }),
+  query("limit").customSanitizer((value) => (value ? value : 5)),
+  query("page", "page must be a valid integer")
+    .optional()
+    .isInt()
+    .toInt({ min: 1 }),
+  query("page").customSanitizer((value) => (value ? value : 1)),
   query("order", "order must be one of ['asc', 'dsc']")
     .optional()
     .isIn(["asc", "dsc"]),
+  query("order").customSanitizer((value) => (value ? value : "asc")),
 ];
 
 module.exports = getTasksValidator;
