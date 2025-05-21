b97534a458656ce1a87b96b74ada5a0c98170b91
Victor Seleimend <sseleimend@gmail.com>
2025-05-21 10:32:08 -0300

    Update the response formatter by refactoring the response

--- src/middleware/responseFormatter.middleware.js
+++ src/middleware/responseFormatter.middleware.js
@@ -9,9 +9,19 @@ function responseFormatter(req, res, next) {
         res.statusCode >= 200 && res.statusCode < 300 ? "success" : "error",
       statusCode: res.statusCode,
       message: getReasonPhrase(res.statusCode),
-      data: res.statusCode >= 200 && res.statusCode < 300 ? data : null,
-      error: res.statusCode >= 200 && res.statusCode < 300 ? null : data,
     };
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      response.data = data.pagination ? data.data : data;
+    }
+
+    if (res.statusCode > 300) {
+      response.error = data;
+    }
+
+    if (data.pagination) {
+      response.pagination = data.pagination;
+    }
 
     originalJson.call(res, response);
   };
