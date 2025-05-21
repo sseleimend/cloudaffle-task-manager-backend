27862c1fb470c5e3407a985b5696fafc30b92a41
Victor Seleimend <sseleimend@gmail.com>
2025-05-21 12:33:45 -0300

    Add authenticateToken middleware

---
+++ src/middleware/authenticateToken.middleware.js
@@ -0,0 +1,23 @@
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "You are not authorized to perform this request",
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(StatusCodes.FORBIDDEN).json({
+        message: "Please login again, invalid token.",
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
