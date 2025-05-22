e341d9a7033844d79208fbc2d50c05d5eab01e97
Victor Seleimend <sseleimend@gmail.com>
2025-05-22 10:53:08 -0300

    Add documentation to the user's POST endpoint

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -6,6 +6,55 @@ const { validationResult } = require("express-validator");
 
 const usersRouter = express.Router();
 
+/**
+ * @swagger
+ *
+ * /users/create:
+ *   post:
+ *    summary: Create a new user
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      201:
+ *        description: User created successfully
+ *        content:
+ *          application/json:
+ *            example:
+ *              status: success
+ *              statusCode: 201
+ *              message: Created
+ *              data:
+ *                _id: 682df37f265c26067b3b7ece
+ *                firstName: John
+ *                lastName: Doe
+ *                email: john@doe.com
+ *      401:
+ *        description: Not authorized error
+ *        content:
+ *          application/json:
+ *            example:
+ *              status: error
+ *              statusCode: 401
+ *              message: Unauthorized
+ *              error:
+ *                message: You are not authorized to perform this request
+ *      403:
+ *        description: Forbidden error
+ *        content:
+ *          application/json:
+ *            example:
+ *              status: error
+ *              statusCode: 403
+ *              message: Forbidden
+ *              error:
+ *                message: Please login again, ivalid token
+ */
+
 usersRouter.post("/create", createUserValidator, (req, res) => {
   const result = validationResult(req);
 
