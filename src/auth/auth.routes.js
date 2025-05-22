03666cd1243583bc8da334a9f63c70ca773461d2
Victor Seleimend <sseleimend@gmail.com>
2025-05-22 11:00:52 -0300

    Add documentation to the login endpoint

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -6,6 +6,32 @@ const loginValidator = require("./validators/login.validator.js");
 
 const authRouter = express.Router();
 
+/**
+ * @swagger
+ *
+ * /auth/login:
+ *   post:
+ *    summary: User login
+ *    tags: [Authentication]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Login'
+ *    responses:
+ *      200:
+ *        description: User login successfully
+ *        content:
+ *          application/json:
+ *            example:
+ *              status: success
+ *              statusCode: 200
+ *              message: Ok
+ *              data:
+ *                accessToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2ODJkZGViMTZmZGRhYTUyNTAyMzg5N2EiLCJlbWFpbCI6ImpvaG5AZG9lLmNvbSIsImlhdCI6MTc0NzkyMjIyNiwiZXhwIjoxNzQ4MDA4NjI2fQ.EjRO5G9b5UGckD16xtSTWiAhfsDWjkbEAv9icfn4-Pw
+ */
+
 authRouter.post("/login", loginValidator, (req, res) => {
   const result = validationResult(req);
 
@@ -17,3 +43,25 @@ authRouter.post("/login", loginValidator, (req, res) => {
 });
 
 module.exports = authRouter;
+
+/**
+ * @swagger
+ *
+ * components:
+ *  schemas:
+ *    Login:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *      properties:
+ *        email:
+ *          type: string
+ *          description: A valid email address
+ *        password:
+ *          type: string
+ *          description: Must contain 8 characters and also a number, a capital letter and a special character
+ *      example:
+ *        email: john@doe.com
+ *        password: Test123#
+ */
